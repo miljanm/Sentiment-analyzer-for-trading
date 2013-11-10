1c781d451d292b08aec7fce32ee3c6826ce5163b
miljan <miljan.martic@gmail.com>
2013-11-10 01:52:57 +0000

    -plotting module -classification module upgrades -non-farm payrole datasets and tests

--- My Project/TwitterCrawler/Classification.py
+++ My Project/TwitterCrawler/Classification.py
@@ -6,7 +6,7 @@ Created on 8 Nov 2013
 
 from FeatureDetection import detectFeatures
 from NaiveBayes import classifyTweet
-import pprint, csv
+import pprint, csv  # @UnusedImport
 
 
 """
@@ -15,11 +15,11 @@ and marks all the important features present in the given tweets.
 It produces an output csv file with feature vector added at the end
 of each tweet.
 """
-def analyzeTweets(pairname):
+def analyzeTweets(pairname,infile, outfile):
     # list of tweets in the set
     l_tweets = []
     # Read all the data from the designated file
-    path1 = "NewData/" + pairname + "/" + pairname + "test1.csv"
+    path1 = "NewData/" + pairname + "/" + pairname + infile
     with open(path1, 'rb') as csvfile1:
         reader1 = csv.reader(csvfile1, delimiter=',')
         for row in reader1:
@@ -31,28 +31,37 @@ def analyzeTweets(pairname):
         tweet.append(l_features)
 #     pprint.pprint(l_tweets)
     # write the result to a csv file
-    resultFile = open("NewData/" + pairname + "/" + pairname + "output1.csv",'wb')
+    resultFile = open("NewData/" + pairname + "/" + pairname + outfile,'wb')
     wr = csv.writer(resultFile, dialect='excel')
     wr.writerows(l_tweets)
 
-
-def classify(pairname):
-    featureVectors = []
-    path1 = "NewData/" + pairname + "/" + pairname + "output1.csv"
+"""
+Function which takes name of the fx pair and input and output files.
+It classifies all the tweets in the input file of the given pair
+and write results to the specified outfile.
+"""
+def classify(pairname,infile, outfile):
+    data = []
+    path1 = "NewData/" + pairname + "/" + pairname + infile
     with open(path1, 'rb') as csvfile1:
         reader1 = csv.reader(csvfile1, delimiter=',')
         for row in reader1:
-            featureVectors.append(row[4])
+            data.append(row)
     
-    classifications = []
-    for i in featureVectors:
-        classifications.append(classifyTweet(i, pairname))
-    pprint.pprint(classifications)
+    # append classifications to tweets
+    for i in data:
+        classification = classifyTweet(i[4], pairname)
+        i.append(classification[0])
+    
+    # write classifications to a specified file
+    resultFile = open("NewData/" + pairname + "/" + pairname + outfile,'wb')
+    wr = csv.writer(resultFile, dialect='excel')
+    wr.writerows(data)
             
 
 if __name__ == '__main__':
-#     analyzeTweets('EURUSD')
-    classify('EURUSD')
+#     analyzeTweets('EURUSD', '_NFPtest8Nov.csv', '_NFPtest8NovFeatures.csv')
+    classify('EURUSD', '_NFPtest8NovFeatures.csv', '_NFPtest8NovClassified.csv')
     
     
     
---
+++ My Project/TwitterCrawler/Plot.py
@@ -0,0 +1,120 @@
+'''
+Created on 9 Nov 2013
+
+@author: miljan
+'''
+
+import matplotlib.dates as dt
+import matplotlib.pyplot as plot
+import csv, pprint  # @UnusedImport
+from datetime import datetime
+import numpy as np
+
+"""
+Function which takes a date in the format provided from fx quotes
+from yahoo finance and returns a matplotlib date object for plotting.
+"""
+def _formatDate(dateString):
+    try:
+        date_object = datetime.strptime(dateString, '%d/%m/%Y %H:%M:%S')
+    except Exception as e:
+        print e
+        return 'FAIL'
+    return dt.date2num(date_object)
+
+"""
+Function which returns a numpy array with price data for the 
+requested pair.
+"""
+def getPriceData(pairname):
+    data = []
+    path = "NewData/" + pairname + "/" + pairname + "Prices.csv"
+    with open(path, 'rb') as csvfile1:
+        reader1 = csv.reader(csvfile1, delimiter=',')
+        for row in reader1:
+            date = _formatDate(row[1])
+            # guard against improperly formatted time strings
+            if date == 'FAIL':
+                continue
+            arr = np.array([float(row[0]),date])
+            data.append(arr)
+    # convert price/dates pairs to numpy array
+    return np.array(data)
+
+def getSentimentData(pairname, infile):
+    data = []#
+    path = "NewData/" + pairname + "/" + pairname + infile
+    with open(path, 'rb') as csvfile1:
+        reader1 = csv.reader(csvfile1, delimiter=',')
+        for row in reader1:
+            date = _formatDate(row[3])
+            # guard against improperly formatted time strings
+            if date == 'FAIL':
+                continue
+            arr = np.array([int(row[5]),date])
+            data.append(arr)
+        # make continuous time series for the sentiment
+        for i in xrange(1,len(data)):
+            data[i][0] += data[i-1][0]
+    return np.array(data)
+
+
+"""
+Function which plots the price data correlated with sentiment data.
+"""
+def plotPrediction(np_price_data, np_sentiment_data):
+#     # plot the prices 
+#     fig, ax = plot.subplots()
+#     plot.plot_date(np_price_data[-3000:-1000,1],np_price_data[-3000:-1000,0],'-')
+# #     plot.plot_date(np_price_data[:,1],np_price_data[:,0],'-')
+#     plot.plot_date(np_sentiment_data[:,1],np_sentiment_data[:,0],'-')
+#     ax.autoscale_view()
+#     ax.grid(True)
+#     fig.autofmt_xdate()
+#     plot.show()
+    
+    from mpl_toolkits.axes_grid1 import host_subplot
+
+#     fig, ax = plot.subplots()
+    host = host_subplot(111)
+    par = host.twinx()
+
+    host.set_xlabel("Time")
+    host.set_ylabel("Pair Price")
+    par.set_ylabel("Sentiment")
+      
+    p1, = host.plot_date(np_price_data[-3000:-1000,1],np_price_data[-3000:-1000,0],'-',label='Pair Price')
+    p2, = par.plot_date(np_sentiment_data[:,1],np_sentiment_data[:,0],'-',label='Sentiment')
+    
+    host.autoscale_view()
+    host.grid(True)
+#     par.set_ylim([-2000,500])
+#     fig.autofmt_xdate()
+#     matplotlib.pyplot.autofmt_xdate()
+    plot.xticks(rotation=40)
+    
+     
+    leg = plot.legend(loc='lower right')
+      
+    host.yaxis.get_label().set_color(p1.get_color())
+    leg.texts[0].set_color(p1.get_color())
+       
+    par.yaxis.get_label().set_color(p2.get_color())
+    leg.texts[1].set_color(p2.get_color())
+     
+#     plot.show()
+    
+    fig = plot.gcf()
+    fig.set_size_inches(18.5,10.5)
+    plot.savefig('NFPtest.png',dpi=100)
+
+    
+if __name__ == '__main__':
+    np_sentiment_data = getSentimentData('EURUSD', '_NFPtest8NovClassified.csv')
+    np_price_data = getPriceData('EURUSD')
+    plotPrediction(np_price_data, np_sentiment_data)
+    
+    
+    
+    
+    
